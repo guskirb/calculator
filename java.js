9c31633a602c2a11d5603a960297d0d7789a10c3
guskirb <anguskirby1998@gmail.com>
2024-04-03 22:00:24 +0100

    Complete basic operation fuctions and fix bugs

--- java.js
+++ java.js
@@ -1,9 +1,10 @@
 const numButton = document.querySelectorAll(".number");
-const addButton = document.querySelector(".buttons .add");
-const minusButton = document.querySelector(".buttons .minus");
-const divideButton = document.querySelector(".buttons .divide");
-const multiplyButton = document.querySelector(".buttons .multiply");
+const addButton = document.querySelector(".add");
+const minusButton = document.querySelector(".minus");
+const divideButton = document.querySelector(".divide");
+const multiplyButton = document.querySelector(".multiply");
 const clearButton = document.querySelector(".clear");
+const equalsButton = document.querySelector(".equals")
 
 let displayValueBig = document.getElementById("bigNum");
 let displayValueSmall = document.getElementById("smallNum");
@@ -11,27 +12,37 @@ let currentValue = "";
 let valueTwo = "";
 let operation = null;
 
-
-
-function addition(valueOne, valueTwo) {
-    currentValue = parseInt(valueOne) + parseInt(valueTwo);
+function addition() {
+    valueTwo = parseInt(currentValue) + parseInt(valueTwo);
+    valueTwo = Math.round(valueTwo * 100) / 100;
+    return valueTwo;
 }
 
-function subtract(valueOne, valueTwo) {
-    currentValue = parseInt(valueOne) - parseInt(valueTwo);
+function subtract() {
+    valueTwo = parseInt(valueTwo) - parseInt(currentValue);
+    valueTwo = Math.round(valueTwo * 100) / 100;
+    return valueTwo;
 }
 
-function multiply(valueOne, valueTwo) {
-    currentValue = parseInt(valueOne) * parseInt(valueTwo);
+function multiply() {
+    valueTwo = parseInt(valueTwo) * parseInt(currentValue);
+    valueTwo = Math.round(valueTwo * 100) / 100;
+    return valueTwo;
 }
 
-function divide(valueOne, valueTwo) {
-    currentValue = parseInt(valueOne) / parseInt(valueTwo);
+function divide() {
+    valueTwo = parseInt(valueTwo) / parseInt(currentValue);
+    valueTwo = Math.round(valueTwo * 100) / 100;
+    return valueTwo;
 }
+
+
 
 function operate(valueOne, valueTwo, operation) {
     operation(valueOne, valueTwo);
 }
+
+// Clears the screen and all values
 
 function clear() {
     currentValue = "";
@@ -41,7 +52,7 @@ function clear() {
     displayValueSmall.textContent = "";
 }
 
-
+// Number button that adds to value
 
 numButton.forEach(element => {
     element.addEventListener("click", () => {
@@ -54,10 +65,100 @@ numButton.forEach(element => {
         } else {
             currentValue += element.textContent;
             displayValueBig.textContent = currentValue;
+            displayValueSmall.textContent += element.textContent;;
         }
     });
 });
 
 clearButton.addEventListener("click", () => {
     clear();
+});
+
+function storeValue() {
+    valueTwo = currentValue;
+    displayValueSmall.textContent = valueTwo;
+    currentValue = "";
+    displayValueBig.textContent = "0";
+};
+
+// Operations
+
+addButton.addEventListener("click", () => {
+    if (valueTwo === "") {
+        storeValue();
+        displayValueSmall.textContent += "+";
+    } if (operation === null) {
+        operation = addition;
+    } if (currentValue === "") {
+        currentValue = valueTwo;
+        operation = addition;
+    } else {
+        // displayValueSmall.textContent += valueTwo;
+        displayValueSmall.textContent += "+";
+        operation();
+        operation = addition;
+        displayValueBig.textContent = valueTwo;
+        currentValue = "";
+    }
+});
+
+minusButton.addEventListener("click", () => {
+    if (valueTwo === "") {
+        storeValue();
+        displayValueSmall.textContent += "-";
+    } if (operation === null) {
+        operation = subtract;
+    } else {
+
+        operation();
+        displayValueSmall.textContent += currentValue;
+        displayValueSmall.textContent += "-";
+
+        operation = subtract;
+        currentValue = "";
+    }
+});
+
+multiplyButton.addEventListener("click", () => {
+    if (valueTwo === "") {
+        storeValue();
+        displayValueSmall.textContent += "×";
+    } if (operation === null) {
+        operation = multiply;
+    } else {
+        displayValueSmall.textContent += valueTwo;
+        displayValueSmall.textContent += "×";
+        operation();
+        operation = multiply;
+        currentValue = "";
+    }
+});
+
+divideButton.addEventListener("click", () => {
+    if (valueTwo === "") {
+        storeValue();
+        displayValueSmall.textContent += "÷";
+    } if (operation === null) {
+        operation = divide;
+    } else {
+        displayValueSmall.textContent += valueTwo;
+        displayValueSmall.textContent += "÷";
+        operation();
+        operation = divide;
+        currentValue = "";
+    }
+});
+
+equalsButton.addEventListener("click", () => {
+    if (valueTwo === "") {
+        return;
+    } else {
+        operation();
+        displayValueSmall.textContent += "=";
+        displayValueSmall.textContent += valueTwo;
+        currentValue = valueTwo;
+        displayValueBig.textContent = currentValue;
+        operation = null;
+        valueTwo = "";
+    }
 });
